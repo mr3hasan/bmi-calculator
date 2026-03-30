b69f03f05778f3d277c1a8f31fef458ca9fc66d9
mr3hasan <124229254+mr3hasan@users.noreply.github.com>
2026-03-30 14:45:19 +0600

    Create app.config.js

---
+++ app.config.js
@@ -0,0 +1,31 @@
+export default {
+  expo: {
+    name: "BMI Calculator",
+    slug: "bmi-calculator",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#e2e8f0",
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#e2e8f0",
+      },
+      package: "com.mr3hasan.bmicalculator",
+    },
+    ios: {
+      bundleIdentifier: "com.mr3hasan.bmicalculator",
+      supportsTablet: true,
+    },
+    extra: {
+      eas: {
+        projectId: "564ff295-0168-4c94-873e-21722dcf2519",
+      },
+    },
+  },
+};
