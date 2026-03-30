bab316fd90024dff9d08d9fd8ad523bf54a254f3
mr3hasan <124229254+mr3hasan@users.noreply.github.com>
2026-03-30 14:56:20 +0600

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   expo: {
     name: "BMI Calculator",
     slug: "bmi-calculator",
@@ -9,23 +9,23 @@ export default {
     splash: {
       image: "./assets/splash-icon.png",
       resizeMode: "contain",
-      backgroundColor: "#e2e8f0",
+      backgroundColor: "#e2e8f0"
     },
     android: {
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#e2e8f0",
+        backgroundColor: "#e2e8f0"
       },
-      package: "com.mr3hasan.bmicalculator",
+      package: "com.mr3hasan.bmicalculator"
     },
     ios: {
       bundleIdentifier: "com.mr3hasan.bmicalculator",
-      supportsTablet: true,
+      supportsTablet: true
     },
     extra: {
       eas: {
-        projectId: "564ff295-0168-4c94-873e-21722dcf2519",
-      },
-    },
-  },
+        projectId: "564ff295-0168-4c94-873e-21722dcf2519"
+      }
+    }
+  }
 };
